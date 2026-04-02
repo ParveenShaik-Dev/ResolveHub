1703f98c7dddca60262bf83a4faefd20faf66025
Parveen <shaik.p4151@gmail.com>
2026-04-02 22:34:18 +0530

    Switch to SQLite database

--- SupportTicket.API/Program.cs
+++ SupportTicket.API/Program.cs
@@ -48,7 +48,8 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+  // options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+  options.UseSqlite("Data Source=ticketdb.sqlite"));
 
 //  Repositories  add these lines in Program.cs after builder.Services.AddDbContext
 builder.Services.AddScoped<IUserRepository, UserRepository>();
